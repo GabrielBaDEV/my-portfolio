6a18259f280a9891a9b18d98678c5c77b79b753b
Gabriel Barbosa <gabrielsaba30@hotmail.com>
2023-07-16 16:58:14 -0300

    contactfix

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ function sendMail() {
 
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Sua mensagem foi enviada!")
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
 
     })
     .catch(err=>console.log(err));
