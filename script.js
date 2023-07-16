fe018c9f9352b3af2ded8cfd943839555a93fcdc
Gabriel Barbosa <gabrielsaba30@hotmail.com>
2023-07-16 16:29:18 -0300

    contactwork

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+function sendMail() {
+    const params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_vfzmt34";
+    const templateID = "template_1mjo4ek";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
